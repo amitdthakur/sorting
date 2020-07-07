e21a4f08c71244ec89faee56e56ea757d1767421
Amit <thakuramit@gmail.com>
2020-07-07 23:19:21 +0530

    Corrected selection sort logic

--- src/main/java/com/java/sorting/selectionsort/SelectionSort.java
+++ src/main/java/com/java/sorting/selectionsort/SelectionSort.java
@@ -7,20 +7,20 @@ package com.java.sorting.selectionsort;
  */
 public class SelectionSort {
 
-  public static void selectionSort(int[] inputs) {
+  public static void selectionSort(int[] inputs, int length) {
     // Running till second last element as till reaching last element everything will be sorted.
-    for (int i = 0; i < inputs.length - 1; i++) {
+    for (int i = 0; i < length - 1; i++) {
       int minIndex = i;
-      for (int j = i + 1; j < inputs.length; j++) {
+      for (int j = i + 1; j < length; j++) {
         // checking for minimum value in array
         if (inputs[j] < inputs[minIndex]) {
           minIndex = j;
         }
-        // Swap value.
-        int temp = inputs[minIndex];
-        inputs[minIndex] = inputs[i];
-        inputs[i] = temp;
       }
+      // Swap value.
+      int temp = inputs[i];
+      inputs[i] = inputs[minIndex];
+      inputs[minIndex] = temp;
     }
   }
 
